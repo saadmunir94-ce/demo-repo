e60c8ee62ad431ee98a8095af38be823d9648841
Saad Munir <smunir1994@gmail.com>
2022-04-25 22:00:15 +0200

    type hints

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 class Dog:
     def __init__(self, color):
         self.color = color
-    def get_color(self):
+    def get_color(self) -> str:
         return self.color
 dog = Dog("blue")
 print("Dog has color {}".format(dog.get_color()))
